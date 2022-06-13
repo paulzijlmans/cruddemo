05220ac6d7112c3240f14187f4163ca5701568cc
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-06-13 16:31:17 +0200

    Spring Data REST

--- src/main/java/nl/paulzijlmans/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/nl/paulzijlmans/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package nl.paulzijlmans.springboot.cruddemo.dao;
 
 import nl.paulzijlmans.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
